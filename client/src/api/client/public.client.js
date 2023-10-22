b12899e054a07a6ec9014551868e5ed78380473c
Aarya Reddy <b.aarya.reddy@gmail.com>
2023-10-21 19:10:43 -0700

    fix final

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
-// const baseURL = "https://krait-movie-api.vercel.app/api/v1/";
+// const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://krait-mv-api.vercel.app/api/v1/";
 // const baseURL = "https://127.0.0.1:5000/api/v1/";
+// const baseURL = "https://localhost:3000/api/v1/";
+
 
 
 const publicClient = axios.create({
